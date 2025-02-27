fd0af83eab0b221f16303a329f986c8bb53ba4cf
emilysteed <ebradfield@rti.org>
2025-02-27 11:04:37 -0500

    adding base page and necessary files

---
+++ app.py
@@ -0,0 +1,21 @@
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+from components.sidebar import sidebar
+
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+)
+
+app.layout = html.Div([
+         dcc.Location(id="url", pathname="/02_usaid"),
+         sidebar(),
+         html.Div(className="page-content"),
+         dash.page_container,
+     ],
+ )
+
+if __name__ == '__main__':
+    app.run(debug=True, dev_tools_hot_reload=True)
